77e458c9a4fa28d883d6921372dad4450b30192a
Agusgal <agaldeman@yahoo.com.ar>
2021-05-07 22:18:58 -0300

    minor changes

---
+++ Psola.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import librosa as lr
+
+if __name__ == '__main__':
+
+    audio, fs = lr.load('04 Harder, Better, Faster, Stronger.wav', 44100)
+
+    time = np.arange(0, len(audio)) / fs
+
+    fig, ax = plt.subplots()
+    ax.plot(time, audio)
+    ax.set(xlabel='time (s)', ylabel='Sound amplitude')
+    plt.show()
+
+
+
+
