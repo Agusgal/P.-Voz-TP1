5efde17734e9a52634a988a15155531f9032e361
Agusgal <agaldeman@yahoo.com.ar>
2021-05-14 23:09:31 -0300

    b

--- Psola.py
+++ Psola.py
@@ -6,20 +6,9 @@ import librosa as lr
 
 if __name__ == '__main__':
 
-    audio, fs = lr.load('04 Harder, Better, Faster, Stronger.wav', sr=44100, offset=0.1, duration=20)
-
-    print(audio)
-
-    r = autocorr(audio)
-    print(r)
+    arr = np.array([1, 2, 3])
+    print(np.correlate(arr, arr, 'full'))
 
 
-    time = np.arange(0, len(audio)) / fs
-
-    fig, ax = plt.subplots()
-    ax.plot(time, audio)
-    ax.set(xlabel='time (s)', ylabel='Sound amplitude')
-    plt.show()
-    pass
 
 
