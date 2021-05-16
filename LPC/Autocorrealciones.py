1d1df3779888c140f05388439e9bd65c0c8132f4
Agusgal <agaldeman@yahoo.com.ar>
2021-05-16 01:20:04 -0300

    a

--- LPC/Autocorrealciones.py
+++ LPC/Autocorrealciones.py
@@ -1,37 +1,91 @@
+from scipy.linalg import solve_toeplitz, toeplitz
+import librosa
+
+
 import numpy as np
-from scipy.fftpack import fft, ifft
+import matplotlib.pyplot as plt
+from scipy import signal
 
 
-
-def autocorr(signal):
+def corr_short_time(sgn, n, L):
     """
+    Parameters
+    ----------
+    sgn: np.array, señal a analizar
+    n:   escalar, punto donde se calcula (n hat)
+    L:   tamaño de bloque (ms)
 
-    :param signal:
-    :return:
+    Return
+    ----------
+    retorna correlacion short time en instante n
     """
-    maxlag = signal.shape[-1]
-    fftPoints = nextpow2(2*maxlag - 1)
-    a = computeAcorr(signal, fftPoints, maxlag)
-    return a
+    L *= 10e-3 * 44100
+
+    # ventana rectangular
+    window = [i >= n and i <= n + L - 1 for i in range(len(sgn))]
+
+    # arreglo nuevo
+    arr_nuevo = np.multiply(sgn, window)
+    corr = signal.correlate(arr_nuevo, arr_nuevo, 'full')
+
+    return corr
 
 
-def computeAcorr(signal, fftpoints, maxlag):
-    a = np.real(ifft(np.abs(fft(signal, n=fftpoints) ** 2))) ##Aca no se si dividir por el tamaño
-    return a[..., :maxlag + 1]
+def LPC(sgn, M):
+    r = corr_short_time(sgn, len(sgn) // 2, 20)
+    center_r = len(r) // 2
+
+    r_M = r[center_r: center_r + M]
+    r_M2 = r[center_r + 1: center_r + M + 1]
+
+    alphas = solve_toeplitz(r_M, r_M2)
+    return alphas
 
 
-def nextpow2(n):
-    """
-    Devuelve la potencia de 2 tal que 2^p >= n
+def plot_psd(signals, fs, Ms):
+    plt.figure(figsize=(12, 12))
+    plt.grid(which='both')
 
-    :param n:
-    :return:
-    """
+    plt.title('Espectro de señal de error para diferentes filtros')
 
-    f, p = np.frexp(n)
-    if f == 0.5:
-        return p - 1
-    elif np.isfinite(f):
-        return p
-    else:
-        return f
+    for signal, M in zip(signals, Ms):
+        signal_asd = np.fft.fft(signal, n=len(signal))
+
+        signal_psd = np.abs(signal_asd) ** 2
+
+        freqs = np.fft.fftfreq(len(signal), d=1 / fs)
+
+        plt.semilogx(freqs, signal_psd, label=f'M = {M}')
+
+    plt.legend()
+
+if __name__ == '__main__':
+    filename = 'DaftPunk.wav'
+    filename2 = '../Resources/Lorde.wav'
+    sample, sr = librosa.load(filename2, sr=44100)
+
+    plt.plot(np.linspace(0, 30, len(sample)), sample)
+    plt.show()
+
+    alphas = LPC(sample, 20)
+
+    b = np.hstack([[0], -1 * alphas[1:]])
+    sample_hat = signal.lfilter(b, [1], sample)
+
+    fig, ax = plt.subplots()
+    ax.plot(sample)
+    ax.plot(sample_hat, linestyle='--')
+    ax.legend(['y', 'y_hat'])
+    ax.set_title('LP Model Forward Prediction')
+    plt.show()
+
+    b2 = np.hstack([[1], alphas[1:]])
+    err = signal.lfilter(b2, [1], sample)
+    plt.plot(np.linspace(0, 30, len(sample)), err)
+    plt.show()
+
+    alphasLibrosa = librosa.lpc(sample, 5)
+    b3 = np.hstack([[1], -1 * alphasLibrosa[1:]])
+    sampleLibrosa = signal.lfilter(b3, [1], sample)
+
+
--- Psola.py
+++ Psola.py
@@ -7,6 +7,13 @@ import librosa as lr
 if __name__ == '__main__':
 
     arr = np.array([1, 2, 3])
+
+    arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+    r = np.zeros(1, arr2.dtype)
+    print(r)
+    print(arr2.dtype)
+
     print(np.correlate(arr, arr, 'full'))
 
 
